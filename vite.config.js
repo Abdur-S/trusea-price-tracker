5176209547295ca6a2cd881cbadf2b5cf454f4d5
Abdur-S <145555673+Abdur-S@users.noreply.github.com>
2026-07-01 14:31:56 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // This line tells Vite to use the exact repository name as the base path for GitHub Pages
-  base: '/trusea-price-tracker/',
-})
